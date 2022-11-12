2af55a4ecc619c62878c7bafbfe3c6f9a8eeb3ac
shelterisland23 <71450573+shelterisland23@users.noreply.github.com>
2022-11-12 16:42:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,22 +27,18 @@ const USER_CONFIG = {
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-20',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小马', year: '2000', date: '08-20',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '生日', name: '阿兹', year: '1999', date: '11-05',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2020-10-28' },
+        { keyword: 'marry_day', date: '2020-10-18' },
       ],
     },
   ],
